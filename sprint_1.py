75d203d7eabb5ad2a04d64651d71fe871886382c
celestialcomets <katiedangto@gmail.com>
2023-03-08 01:24:09 -0600

    added captain apis
    need to test and verify apis

--- sprint_1.py
+++ sprint_1.py
@@ -8,6 +8,61 @@ import flask
 from flask import jsonify
 from flask import request
 
+# finished, need to test and write notes
+@app.route('/api/captain', methods=["GET"])
+def view_all_captains():
+    myCreds = creds.Creds()
+    conn = create_con(myCreds.connectionstring, myCreds.username, myCreds.passwd, myCreds.dataBase)
+    sql ="select * from captain"
+    users = execute_read_query(conn, sql)
+    return jsonify(users)
+
+# finished, need to test and write notes
+@app.route('/api/captain', methods=["POST"])
+def add_captain():
+    request_data = request.get_json()
+    firstname = request_data['firstname']
+    lastname = request_data['lastname']
+    rank = request_data['rank']
+    homeplanet = request_data['homeplanet']
+
+    myCreds = creds.Creds()
+    connection = create_con(myCreds.connectionstring, myCreds.username, myCreds.passwd, myCreds.dataBase)
+    sql = "insert into captain(firstname, lastname, rank, homeplanet) values ('%s','%s','%s', '%s')" % (firstname, lastname, rank, homeplanet)
+    execute_query(connection, sql)
+
+    return 'New Captain Added!'
+
+# finished, need to test and write notes
+@app.route('/api/captain', methods=["PUT"])
+def update_captain():
+    request_data = request.get_json()
+    id = request_data['id']
+    firstname = request_data['firstname']
+    lastname = request_data['lastname']
+    rank = request_data['rank']
+    homeplanet = request_data['homeplanet']
+
+    myCreds = creds.Creds()
+    connection = create_con(myCreds.connectionstring, myCreds.username, myCreds.passwd, myCreds.dataBase)
+    sql = "UPDATE captain SET firstname = '%s', lastname = '%s', rank = '%s', homeplanet = '%s' WHERE id = '%s'" % (firstname, lastname, rank, homeplanet, id)
+    execute_query(connection, sql)
+
+    return f'Captain {firstname} Updated!'
+
+# finished, need to test and write notes
+@app.route('/api/captain', methods=['DELETE'])
+def delete_captain():
+    request_data = request.get_json()
+    delete = request_data['id']
+    
+    myCreds = creds.Creds()
+    connection = create_con(myCreds.connectionstring, myCreds.username, myCreds.passwd, myCreds.dataBase)
+    sql = "delete from captain where id = %s" % (delete)
+    execute_query(connection, sql)
+        
+    return f"Captain {delete} Deleted!"
+
 # finished, add notes and test
 @app.route('/api/cargo', methods=["GET"])
 def view_all_cargo():
