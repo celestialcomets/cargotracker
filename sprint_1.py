c1a6970b46930c7a3531b4d9660d1cbc2f5fa9c2
celestialcomets <katiedangto@gmail.com>
2023-03-08 01:37:43 -0600

    added spaceship apis
    need to test and verify apis

--- sprint_1.py
+++ sprint_1.py
@@ -63,6 +63,65 @@ def delete_captain():
         
     return f"Captain {delete} Deleted!"
 
+# finished, need to test and write notes
+@app.route('/api/spaceship', methods=["GET"])
+def view_all_spaceships():
+    myCreds = creds.Creds()
+    conn = create_con(myCreds.connectionstring, myCreds.username, myCreds.passwd, myCreds.dataBase)
+    sql ="select * from spaceship"
+    users = execute_read_query(conn, sql)
+    return jsonify(users)
+
+# finished, need to test and write notes
+@app.route('/api/spaceship', methods=["POST"])
+def add_spaceship():
+    request_data = request.get_json()
+    maxweight = request_data['maxweight']
+    captainid = request_data['captainid']
+
+    myCreds = creds.Creds()
+    connection = create_con(myCreds.connectionstring, myCreds.username, myCreds.passwd, myCreds.dataBase)
+    sql = "select * from captain"
+    captains = execute_read_query(conn, sql)
+    if captainid in captains[id]:
+        sql = "insert into spaceship(maxweight, captainid) values ('%s','%s')" % (maxweight, captainid)
+        execute_query(connection, sql)
+        return 'New Spaceship Added!'
+    else:
+        return f'There is no captain number {captainid}!'
+
+# finished, need to test and write notes
+@app.route('/api/spaceship', methods=["PUT"])
+def update_spaceship():
+    request_data = request.get_json()
+    id = request_data['id']
+    maxweight = request_data['maxweight']
+    captainid = request_data['captainid']
+
+    myCreds = creds.Creds()
+    connection = create_con(myCreds.connectionstring, myCreds.username, myCreds.passwd, myCreds.dataBase)
+    sql = "select * from captain"
+    captains = execute_read_query(conn, sql)
+    if captainid in captains[id]:
+        sql = "UPDATE spaceship SET maxweight = '%s', captainid = '%s' WHERE id = '%s'" % (maxweight, captainid, id)
+        execute_query(connection, sql)
+        return f'Spaceship {id} Updated!'
+    else:
+        return f'There is no captain number {captainid}!'
+
+# finished, need to test and write notes
+@app.route('/api/spaceship', methods=['DELETE'])
+def delete_spaceship():
+    request_data = request.get_json()
+    delete = request_data['id']
+    
+    myCreds = creds.Creds()
+    connection = create_con(myCreds.connectionstring, myCreds.username, myCreds.passwd, myCreds.dataBase)
+    sql = "delete from spaceship where id = %s" % (delete)
+    execute_query(connection, sql)
+        
+    return f"Spaceship {delete} Deleted!"
+
 # finished, add notes and test
 @app.route('/api/cargo', methods=["GET"])
 def view_all_cargo():
