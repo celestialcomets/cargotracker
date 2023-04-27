7a60d1b8ca89c5feba0ebf2c650255b384eb7f1b
celestialcomets <katiedangto@gmail.com>
2023-04-27 00:08:08 -0500

    toggle button added

--- frontend/server.js
+++ frontend/server.js
@@ -13,26 +13,34 @@ app.get('/', function(req, res) {
     res.render("login", {});
 });
 
+app.get('/process_login', function(req, res) {
+    res.render("login", {});
+});
+
 app.post('/process_login', function(req, res){
     // get the username and password from the form.
     var user = req.body.username;
     var pass = req.body.password;
-    // use a get method to use the login api, pass on credentials.
-    axios.get('http://127.0.0.1:5000/authenticatedroute', {
+    axios.all([axios.get('http://127.0.0.1:5000/authenticatedroute', {
         auth: {
           username: user,
           password: pass
         }
-    // if successful, takes you to the next page and sends a success message to console.
-    }).then(function(response) {
-    res.render("test1")
-    console.log('Authenticated');
-    // if unsucessful, sends you back to the login page and sends a failure message to console.
-  }).catch(function(error) {
-    res.render("login")
-    console.log('Error on Authentication');
-  });
-}); 
+    }),
+    axios.get('http://127.0.0.1:5000/api/cargo')])
+    .then(axios.spread((firstResponse, secondResponse) => {  
+
+        var cargoData = secondResponse.data;
+
+        res.render('cargo', {
+            cargo: cargoData,
+        });
+        console.log('Authenticated');
+        })).catch(function(error) {
+            res.render("login")
+            console.log('Error on Authentication');
+        });
+    });
     
 app.listen(8080);
 console.log('8080 is the magic port');
